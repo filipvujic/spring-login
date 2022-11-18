d18eedde05a814adb587691b600c0d8acb99ce69
filipvujic <filip.vujic665@gmail.com>
2022-11-18 16:33:21 +0100

    Added EmailValidator, fixed some bugs with url path, tested user registration

--- src/main/java/com/filipvujic/springlogin/appuser/AppUserService.java
+++ src/main/java/com/filipvujic/springlogin/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "User with email %email not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -20,5 +22,21 @@ public class AppUserService implements UserDetailsService {
         return appUserRepository
                 .findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+        if (userExists) {
+            throw new IllegalStateException("Email already taken.");
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        // TODO: Send confirmation token
+
+        return "it works";
     }
 }
--- src/main/java/com/filipvujic/springlogin/registration/RegistrationService.java
+++ src/main/java/com/filipvujic/springlogin/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.filipvujic.springlogin.registration;
 
+import com.filipvujic.springlogin.appuser.AppUser;
+import com.filipvujic.springlogin.appuser.AppUserRole;
+import com.filipvujic.springlogin.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid.");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
